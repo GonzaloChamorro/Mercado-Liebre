8b0d752c17e231d2159c7bb62592c02b6e33a63b
GonzaloChamorro <chamorrogonzalo23@gmail.com>
2022-08-30 21:41:40 -0300

    ultima modificacioon

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-const express = require("express");
-const path = require("path")
-
+const express = require('express');
 const app = express();
+app.use(express.static('public'));
 
-const publicFolderPath = path.resolve(__dirname, './public');
-app.use( express.static(publicFolderPath) );
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo en el puerto 3000');
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando en http://localhost:3000');
 });
 
-app.get('/', (req,res) => {
-    res.sendFile(path.resolve(__dirname, '/views/home.html'));
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
 });
