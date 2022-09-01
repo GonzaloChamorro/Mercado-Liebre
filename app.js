f6429143182ee7236ebcf8ff0bc4610b5aac34ca
GonzaloChamorro <chamorrogonzalo23@gmail.com>
2022-08-31 21:55:12 -0300

    cambios para heroku

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-app.use(express.static(path.join(__dirname, 'Public')));
+app.use(express.static('Public'));
 
 
 app.listen(process.env.PORT || 3000, ()=>{
